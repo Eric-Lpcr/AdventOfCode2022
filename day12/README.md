c8ce2e46a3cb54207ba99292a72b4a0b60100dfe
Eric-Lpcr <lepicier.eric@gmail.com>
2022-12-12 23:29:31 +0100

    Day 12 solution

---
+++ day12/README.md
@@ -0,0 +1,29 @@
+# [Day 12: Hill Climbing Algorithm](https://adventofcode.com/2022/day/12)
+
+Shortest path? Diskstra? No, ___A*___ !
+
+I remembered [2021 calendar, Day 15: Chiton](https://adventofcode.com/2021/day/15)
+and my [comments](../../AdventOfCode2021/day15):
+
+> Oh no, graph again!
+> And shortest path on a weighted graph... That immediately recalled me Dijkstra study 20 years ago during my late 
+> university, with a professor asking us to unwind the algorithm manually and write down (yes, with a pen and a paper) 
+> all the steps.
+> I think I learned about the algorithm (it stayed in my mind), but I also get traumatized ;-(
+> 
+> And tonight I really don't enjoy implementing such a thing.
+>
+> So for one time, I'll do it like a serious lazy developer in the internet era: let's find a nice solution made by
+> someone else. I'm a bit frustrated, but otherwise I won't succeed.
+> 
+> Better than Dijkstra when you know the exact destination is A* algorithm:
+> [Introduction to the A* Algorithm](https://www.redblobgames.com/pathfinding/a-star/introduction.html).
+>
+> Ok, got an excellent code in [implementation.py](implementation.py) from 
+> [Implementation of A*](https://www.redblobgames.com/pathfinding/a-star/implementation.html#python).
+
+This time I wrote a `HeightGrid` class implementing the contract for the `a_star_search` function, and just called it.
+
+Ok, part 1 solved, one more time not so proud of me but after all, reuse is also part of the job.
+
+Part 2 is easy with this fast algorithm, just need to scan for starting points and apply the same method.
---
+++ day12/day12.py
@@ -0,0 +1,87 @@
+from typing import Tuple, Iterator
+
+from functools import partial
+from implementation import a_star_search, GridLocation, WeightedGraph
+
+
+class HeightGrid(WeightedGraph):
+    def __init__(self, list_of_list):
+        self.data = list_of_list
+        self.size_x = len(list_of_list[0])
+        self.size_y = len(list_of_list)
+
+    def height(self, location: GridLocation):
+        x, y = location
+        return self.data[y][x]
+
+    def in_bounds(self, location: GridLocation) -> bool:
+        (x, y) = location
+        return 0 <= x < self.size_x and 0 <= y < self.size_y
+
+    def passable(self, from_location: GridLocation, to_location: GridLocation) -> bool:
+        return ord(self.height(to_location)) <= ord(self.height(from_location)) + 1
+
+    def neighbors(self, location: GridLocation) -> Iterator[GridLocation]:
+        (x, y) = location
+        neighbors = [(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)]  # E W N S
+        results = filter(self.in_bounds, neighbors)
+        results = filter(partial(self.passable, location), results)
+        return results
+
+    def cost(self, from_location: GridLocation, to_location: GridLocation):
+        return 1
+
+
+def find_goals(height_grid):
+    start = 0, 0
+    end = 0, 0
+    for y, line in enumerate(height_grid.data):
+        if 'S' in line:
+            x = line.index('S')
+            start = x, y
+            line[x] = 'a'
+        if 'E' in line:
+            x = line.index('E')
+            end = x, y
+            line[x] = 'z'
+    return start, end
+
+
+def shortest_path(grid, start, goal):
+    came_from, cost_so_far = a_star_search(grid, start, goal)
+    return cost_so_far[goal]
+
+
+def shortest_path_from_all_start_values(grid, start_value, goal):
+    costs = set()
+    start_locations = list()
+    for y, line in enumerate(grid.data):
+        start_locations.extend([(x, y) for x, value in enumerate(line) if value == start_value])
+    for start in start_locations:
+        came_from, cost_so_far = a_star_search(grid, start, goal)
+        if goal in cost_so_far:
+            costs.add(cost_so_far[goal])
+    return min(costs)
+
+
+def main(filename, testing=False, expected1=None, expected2=None):
+    print(f'--------- {filename}')
+
+    with open(filename) as f:
+        height_grid = HeightGrid([list(line) for line in f.read().splitlines()])
+    start, end = find_goals(height_grid)
+
+    result1 = shortest_path(height_grid, start, end)
+    print(f"Part 1: shortest path length is {result1}")
+    if testing and expected1 is not None:
+        assert result1 == expected1
+
+    result2 = shortest_path_from_all_start_values(height_grid, 'a', end)
+    print(f"Part 2: shortest path length from any 'a' is {result2}")
+    if testing and expected2 is not None:
+        assert result2 == expected2
+
+
+if __name__ == '__main__':
+    main('test.txt', True, 31, 29)
+    main('input.txt')
